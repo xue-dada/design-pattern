f898c0cccc992b3fe009df6b5f25ef74c456f20c
xuejishi <xue867706398>
2020-12-21 23:37:32 +0800

    # 开闭原则

---
+++ src/main/java/com/xuejishi/design/principle/openclose/ICourse.java
@@ -0,0 +1,12 @@
+package com.xuejishi.design.principle.openclose;
+
+/**
+ * Created by geely
+ */
+public interface ICourse {
+    Integer getId();
+    String getName();
+    Double getPrice();
+
+
+}
